ce0e6b01ac075d2369e9c05b5a09c2494bdbcdc5
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-15 22:30:13 +0000

    created route to a single product page

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const StyledNavbar = () => {
-
   return (
-    <nav className='navbar'>
-      <NavLink to="/">Home</NavLink>
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Home
+      </NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/products">Products</NavLink>
     </nav>
-  )
-}
+  );
+};
 
 export default StyledNavbar;
