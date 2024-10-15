3d60e956cacf9ff1312fb0034483391057fd2327
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-15 16:27:41 +0000

    created routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Route, Routes, Link } from "react-router-dom";
-import Home  from "./pages/Home"
-import About from "./pages/About"
-import Products  from "./pages/Products";
-import Login from "./pages/Login" 
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/login" element={<Login />}/>
-  </Routes>; 
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="products" element={<Products />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 }
 
 export default App;
