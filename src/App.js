ba6e407f24f5f7deeb10a4f205911bff94a179d4
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-17 16:25:19 +0000

    final commit with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
@@ -10,9 +11,12 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import SharedProductLayout from "./pages/SharedProductLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons/faSpinner";
+const About = React.lazy(() => import("./pages/About"));
 
 // React Router Version 6 was used in this little project.
- 
+
 function App() {
   const [user, setUser] = useState(null);
 
@@ -20,7 +24,21 @@ function App() {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route
+          path="about"
+          element={
+            <React.Suspense
+              fallback={
+                <div style={{ textAlign: "center", marginTop: "100px" }}>
+                  <FontAwesomeIcon icon={faSpinner} spinPulse size="4x"/>
+                  <p style={{ fontSize: "30px" }}>Loading...</p>
+                </div>
+              }
+            >
+              <About />
+            </React.Suspense>
+          }
+        />
         <Route path="products" element={<SharedProductLayout />}>
           <Route index element={<Products />} />
           <Route path=":productId" element={<SingleProduct />} />
